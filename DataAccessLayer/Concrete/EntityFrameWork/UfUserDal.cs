ce36a2eff76b2cb3d94bdc6d1edf2e974b1ebb43
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-01 15:20:44 +0300

    DataAccessLayer UfUserDal ve  IUserDal oluşturuldu

---
+++ DataAccessLayer/Concrete/EntityFrameWork/UfUserDal.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete.Contexts;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.EntityFrameWork
+{
+    public class UfUserDal : EfBaseRepository<User, AppDbContext>, IUserDal
+    {
+    }
+}
