e5f14bae2b2dc92afc3e54ddc312cb146cbe587f
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-01 20:51:06 +0300

    BussinesLayer da  IUserService oluşturuldu

---
+++ BussinesLayer/Abstract/IUserService.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Abstract
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDetailDto>> GetListAsync();
+
+        Task<UserDto> GetByIdAsync(int id);
+
+        Task<UserDto> AddAsync(UserAddDto userAddDto);
+
+        Task<UserUpdateDto> UpdateAsync(UserUpdateDto userUpdateDto);
+        Task<bool> DeleteAsync(int id);
+    }
+
+}
