121c07ccaf3a4e435bcc8cba1d5d4e4d2f376d02
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-01 15:37:54 +0300

    AppDbContext modifed

---
+++ DataAccessLayer/Concrete/Contexts/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using DataAccessLayer.Concrete.EntityFrameWork.Mapping;
+using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Contexts
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public AppDbContext()
+        {
+
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connString = "Data Source=TANINPC;Initial Catalog=DbEtradeWebWithAPI;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            optionsBuilder.UseSqlServer(connString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+    }
+}
