3ca79e271139d8699af4076088716c47f6f3afc5
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-01 15:15:42 +0300

    IBaseRepository  ve EfBaseRepository oluşturuldu

---
+++ DataAccessLayer/Concrete/EfBaseRepository.cs
@@ -0,0 +1,93 @@
+﻿using EntityLayer.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class EfBaseRepository<TEntity, TDbContext> : IBaseRepository<TEntity>
+        where TEntity : class, IEntity, new()
+        where TDbContext : DbContext, new()
+    {
+
+
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            using (TDbContext context = new TDbContext())
+            {
+                return await context.Set<TEntity>().FindAsync(id);
+            }
+        }
+
+        public async Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TDbContext context = new TDbContext())
+            {
+
+                return filter == null
+                    ? await context.Set<TEntity>().ToListAsync()
+                    : await context.Set<TEntity>().Where(filter).ToListAsync();
+            }
+        }
+
+
+        public async Task<TEntity> AddAsync(TEntity entity)
+        {
+            using (TDbContext context = new TDbContext())
+            {
+
+                await context.Set<TEntity>().AddAsync(entity);
+
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
+        }
+
+
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            using (TDbContext context = new TDbContext())
+            {
+                var deleteEntity = await context.Set<TEntity>().FindAsync(id);
+                bool result = false;
+                if (deleteEntity is not null)
+                {
+                    context.Set<TEntity>().Remove(deleteEntity);
+                    await context.SaveChangesAsync();
+                    result = true;
+                }
+
+                return result;
+
+
+
+            }
+        }
+
+        public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TDbContext context = new TDbContext())
+            {
+                return await context.Set<TEntity>().SingleOrDefaultAsync(filter);
+            }
+        }
+
+
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            using (TDbContext context = new TDbContext())
+            {
+                context.Set<TEntity>().Update(entity);
+                await context.SaveChangesAsync();
+                return entity;
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface IBaseRepository<TEntity> where TEntity : class,IEntity,new()
+    {
+        Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> filter = null);
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter);
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);  
+        Task<TEntity> GetByIdAsync(int id);        
+        Task<bool> DeleteByIdAsync(int id);
+
+
+
+
+    }
+}
