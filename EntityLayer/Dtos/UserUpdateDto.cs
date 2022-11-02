ce75971ce3aa0b166c1a8962a7b2acc524dca0ce
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-03 01:10:34 +0300

    EntityLayer  UserUpdateDto DateOfBirth  DataType belirlendi

--- EntityLayer/Dtos/UserUpdateDto.cs
+++ EntityLayer/Dtos/UserUpdateDto.cs
@@ -1,6 +1,8 @@
 ﻿using EntityLayer.Abstract;
 
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace EntityLayer.Dtos
 {
     public class UserUpdateDto : IDto
@@ -14,6 +16,7 @@ namespace EntityLayer.Dtos
         public string LastName { get; set; }
         public string Password { get; set; }
         public bool Gender { get; set; }
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public string Adress { get; set; }
