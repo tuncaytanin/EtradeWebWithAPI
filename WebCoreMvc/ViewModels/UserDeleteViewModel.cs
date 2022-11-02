65f1963a3fd7ff6c1431f97676bb249f05cd7ae1
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-03 01:27:07 +0300

    UserDeleteViewModel silme için eklendi

---
+++ WebCoreMvc/ViewModels/UserDeleteViewModel.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using EntityLayer.Dtos;
+using System.Collections.Generic;
+
+namespace WebCoreMvc.ViewModels
+{
+    public class UserDeleteViewModel
+    {
+
+        public UserUpdateDto UserUpdateDto { get; set; } = new UserUpdateDto();
+        public List<Gender> Genders { get; set; } = new List<Gender>()
+        {
+                new Gender(){  Id = true, GenderName = "Erkek"  },
+                new Gender(){Id =false,GenderName="Kadın"}
+        };
+    }
+}
