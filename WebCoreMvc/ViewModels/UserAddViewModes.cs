6c4196e63d9be7ccf7ad6e4d5e4ae798e9b01548
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-03 01:11:40 +0300

    UserAddViewModes -> User ekleme işlemleri için

---
+++ WebCoreMvc/ViewModels/UserAddViewModes.cs
@@ -0,0 +1,33 @@
+﻿using EntityLayer.Concrete;
+using EntityLayer.Dtos;
+using EntityLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebCoreMvc.ViewModels
+{
+    public class UserAddViewModes
+    {
+
+
+        //public string UserName { get; set; }
+        //public string FirstName { get; set; }
+
+        //public string LastName { get; set; }
+        //public string Password { get; set; }
+        //public int GenderId { get; set; }
+        //[DataType(DataType.Date)]
+        //public DateTime DateOfBirth { get; set; }
+        //public string Email { get; set; }
+        //public string Adress { get; set; }
+
+        public UserAddDto UserAddDto { get; set; } = new UserAddDto();
+
+        public List<Gender> Genders { get; set; } = new List<Gender>()
+        {
+                new Gender(){  Id = true, GenderName = "Erkek"  },
+                new Gender(){Id =false,GenderName="Kadın"}
+        };
+    }
+}
