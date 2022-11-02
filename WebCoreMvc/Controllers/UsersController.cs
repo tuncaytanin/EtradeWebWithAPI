21589bb4c972342c407c9321e14ef7da12e193df
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-02 23:43:08 +0300

    UsersController  olusturuldu

---
+++ WebCoreMvc/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using EntityLayer.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace WebCoreMvc.Controllers
+{
+    public class UsersController : Controller
+    {
+        #region Define
+        private readonly HttpClient _httpClient;
+        private string url = "https://localhost:44340/api/";
+        #endregion
+
+        #region UsersController Constructor
+
+        public UsersController(HttpClient httpClient)
+        {
+            _httpClient=httpClient;
+        }
+        #endregion
+        public async Task<IActionResult> Index()
+        {
+
+            var users = await _httpClient.GetFromJsonAsync<List<UserDetailDto>>(url + "Users/GetList");
+            return View(users);
+        }
+    }
+}
