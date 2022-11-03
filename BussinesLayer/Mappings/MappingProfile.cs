620154fe0ba24e83874378f75f0693b8de2edf72
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-03 22:24:25 +0300

    AutoMapper eklendi ve dto lar ile user arasında otomatilk mapleme sağlandı

---
+++ BussinesLayer/Mappings/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using EntityLayer.Concrete;
+using EntityLayer.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Mappings
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<UserDetailDto, User>();
+            CreateMap<User, UserDetailDto>();
+
+            CreateMap<User, UserAddDto>().ReverseMap();
+            CreateMap<User, UserUpdateDto>().ReverseMap();
+        }
+    }
+}
