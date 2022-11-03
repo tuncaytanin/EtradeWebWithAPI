98559adceaafc16a0b110668b8bf16fcb9c5de09
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-04 00:07:55 +0300

    UserDto ya Password Eklendi

--- EntityLayer/Dtos/UserDto.cs
+++ EntityLayer/Dtos/UserDto.cs
@@ -21,6 +21,8 @@ namespace EntityLayer.Dtos
         public string Email { get; set; }
         public string Adress { get; set; }
         public string PhoneNumber { get; set; }
+
+        public string Password { get; set; }
         public string Token { get; set; }
         public DateTime? TokenExpireDate { get; set; }
     }
