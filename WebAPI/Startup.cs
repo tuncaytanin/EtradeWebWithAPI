ec0df5c26a7a53dfff425b66953c330d8a504ac6
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-03 22:27:43 +0300

    JWT için startup ve appsetting.json gerekli revizeler edildi

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,8 +1,13 @@
+﻿using AutoMapper;
 using BussinesLayer.Abstract;
 using BussinesLayer.Concrete;
+using BussinesLayer.Mappings;
+using Core.Utilities.Security.Token;
+using Core.Utilities.Security.Token.JWT;
 using DataAccessLayer.Abstract;
 using DataAccessLayer.Concrete.Contexts;
 using DataAccessLayer.Concrete.EntityFrameWork;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,10 +18,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebAPI
@@ -38,14 +45,80 @@ namespace WebAPI
             services.AddDbContext<AppDbContext>(opts => opts.UseSqlServer("Data Source=TANINPC;Initial Catalog=DbEtradeWebWithAPI;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False", options => options.MigrationsAssembly("DataAccessLayer").MigrationsHistoryTable(HistoryRepository.DefaultTableName,"dbo")));
             services.AddControllers();
 
-            // servislerimizi buraya ekliyoruz
-            services.AddTransient<IUserDal, UfUserDal>();
-            services.AddTransient<IUserService, UserService>();
+
             //services.AddTransient<IUserService, UserService>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
+
+                // Swager üzerinden beare yetkilendir butno için eklendi
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name="Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme="Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description="Bearer şeması kullan. JWT Yetkilendirme başlığı\r\n\r\n 'Bearer'[Boşluk] giriniz. Örnek : Bearer 1254545asa5d45a4"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type= ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+
             });
+
+            #region JWT
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.SecurityKey);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+
+                };
+            });
+            #endregion
+
+            #region AutoMapper
+
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            var mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+            #endregion
+            #region Serivces
+            // servislerimizi buraya ekliyoruz
+            services.AddTransient<IUserDal, UfUserDal>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<ITokenService, JwtTokenService>();
+            #endregion  
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +135,7 @@ namespace WebAPI
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
