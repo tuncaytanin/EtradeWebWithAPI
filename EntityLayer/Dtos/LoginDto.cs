1f076313c101b3c6feac2bdf720b1e2d170c7267
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-04 00:07:37 +0300

    EntityLayer de LoginDto oluşturuldu

---
+++ EntityLayer/Dtos/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entity.Abstract;
+
+namespace EntityLayer.Dtos
+{
+    public class LoginDto : IDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public bool IsRememberMe { get; set; }
+    }
+}
