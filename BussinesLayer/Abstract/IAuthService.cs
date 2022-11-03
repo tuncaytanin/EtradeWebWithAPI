b73b9b45169990a45227cb3fee70af460047870a
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-11-04 00:05:06 +0300

    Businnes de  IAuthService oluşturuldu

---
+++ BussinesLayer/Abstract/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Core.Utilities.Responses;
+using EntityLayer.Dtos;
+using System.Threading.Tasks;
+
+namespace BussinesLayer.Abstract
+{
+    public interface IAuthService
+    {
+        Task<ApiDataResponse<UserDetailDto>> LoginAsync(LoginDto loginDto);
+    }
+}
