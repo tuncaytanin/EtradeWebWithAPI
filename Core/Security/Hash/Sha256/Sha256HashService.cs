3de47775100226eb6b3b7fc51e3872ecc1093ca0
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2023-03-09 19:01:11 +0300

    MD% ve SHA256 şifreleme yöntemi eklendi

---
+++ Core/Security/Hash/Sha256/Sha256HashService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.Hash.Sha256
+{
+    public class Sha256HashService : IHashService
+    {
+        public bool CompareHash(string hashText, string plainText)
+        {
+            return CreateHash(plainText) == hashText;
+        }
+
+        public string CreateHash(string plainText)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
